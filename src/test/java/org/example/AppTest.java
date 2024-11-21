5e7320a07535c58455ec9e56302d0b9641ae6b14
Tetiana <tanbondar@gmail.com>
2024-11-21 16:42:04 +0200

    cron

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -13,23 +13,23 @@ public class AppTest {
 
     @Test
     public void shouldRepeatApplication()  {
-        String token = System.getenv("token");
+        //String token = System.getenv("token");
 
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
 
         RequestSpecBuilder builder = new RequestSpecBuilder();
         builder.setBaseUri("https://api.lardi-trans.com/v2");
-        builder.addHeader("Authorization", token);
+        builder.addHeader("Authorization", "2NK0IAW8LP2000004004");
         builder.addHeader("Accept", "application/json");
 
         RequestSpecification spec = builder.build();
 
         given().spec(spec).when()
                 .get("/proposals/my/lorries/published").then()
-                .statusCode(200).log().all();
+                .statusCode(200);
 
         given().spec(spec).contentType("application/json").body("{\"lorryIds\":[297563298516]}").when()
                 .post("/proposals/my/repeat/").then()
-                .statusCode(200).log().all();
+                .statusCode(200);
     }
 }
