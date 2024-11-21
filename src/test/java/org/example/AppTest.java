a4eb827201e289c3ccf85578451deba6325132a7
Tetiana <tanbondar@gmail.com>
2024-11-21 13:55:20 +0200

    cron

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -13,16 +13,13 @@ public class AppTest {
 
     @Test
     public void shouldRepeatApplication()  {
-        String token = System.getProperty("token");
-        String token2 = System.getenv("token");
-        System.out.println(token);
-        System.out.println(token2);
+        String token = System.getenv("token");
 
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
 
         RequestSpecBuilder builder = new RequestSpecBuilder();
         builder.setBaseUri("https://api.lardi-trans.com/v2");
-        builder.addHeader("Authorization", "token");
+        builder.addHeader("Authorization", token);
         builder.addHeader("Accept", "application/json");
 
         RequestSpecification spec = builder.build();
